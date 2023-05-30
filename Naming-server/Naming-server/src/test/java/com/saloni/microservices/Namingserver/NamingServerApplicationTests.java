edf5245297bf1beecf31585b1238f7534a21115a
Saloni <105225675+SaloniBargat@users.noreply.github.com>
2023-05-30 23:43:45 +0530

    Add files via upload

---
+++ Naming-server/Naming-server/src/test/java/com/saloni/microservices/Namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saloni.microservices.Namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
