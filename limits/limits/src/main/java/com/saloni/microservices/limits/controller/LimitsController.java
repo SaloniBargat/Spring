7c87094aeb14c58eb5fd7aaee37a0988c7a6dbf6
Saloni <105225675+SaloniBargat@users.noreply.github.com>
2023-05-30 23:45:45 +0530

    Add files via upload

---
+++ limits/limits/src/main/java/com/saloni/microservices/limits/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.saloni.microservices.limits.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saloni.microservices.limits.Model.Limits;
+import com.saloni.microservices.limits.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits (configuration.getMinimum(),configuration.getMaximum());
+		//return new Limits(1, 1000);
+	}
+}
